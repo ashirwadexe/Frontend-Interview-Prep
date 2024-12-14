a3a1a4561ff30c4f7654fac3538181da8131c56b
ashirwadexe <ashirwadbappy@gmail.com>
2024-12-14 11:10:51 +0530

    added star rating

---
+++ 04-star-rating-component/vite-project/src/App.jsx
@@ -0,0 +1,76 @@
+// import React, { useState } from 'react'
+// import './App.css'
+
+// const App = () => {
+
+//   const [rating, setRating] = useState(0);
+//   const [hover, setHover] = useState(0);
+
+//   return (
+//     <div className='app'>
+//       <h1>Star Rating</h1>
+//       <div>
+//         {
+//           [1, 2, 3, 4, 5].map((num) => (
+//             <button
+//               key={num}
+//               onClick={() => setRating(num)}
+//               onMouseOver={() => setHover(num)}
+//               onMouseLeave={() => setHover(rating)}
+//             >
+//               <span
+//                 className={
+//                   ` star 
+//                     ${num <= ((rating && hover) || hover) ? 'on' : 'off'}
+//                   `
+//                 }
+//               >
+//                 &#9733;
+//               </span>
+//             </button>
+//           ))
+//         }
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div className='app'>
+      <h1>Start rating</h1>
+      <div>
+        {
+          [1,2,3,4,5].map((num) => (
+            <button
+              onClick={() => (setRating(num))}
+              onMouseOver={() => (setHover(num))}
+              onMouseLeave={() => setHover(rating)}
+            >
+              <span
+                className={
+                  ` star
+                    ${num <= ((rating && hover) || hover) ? "on" : "off"}
+                  `
+                }
+              >
+                &#9733;
+              </span>
+            </button>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
