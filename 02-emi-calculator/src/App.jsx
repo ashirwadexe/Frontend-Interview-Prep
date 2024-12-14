ba5e5577d1bce1012fd80b610b410933319980e1
ashirwadexe <ashirwadbappy@gmail.com>
2024-12-14 06:56:40 +0530

    adding 03-todo-app

--- 02-emi-calculator/src/App.jsx
+++ 02-emi-calculator/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cost, setCost] = useState(0);
+  const [downPayment, setDownPayment] = useState(0);
+  const [fee, setFee] = useState(1);
+  const [interset, setInterest] = useState(10);
+  const [tenure, setTenure] = useState(12);
+  const [emi, setEmi] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="max-w-7xl mx-auto px-24 py-8">
+          <h1 className="text-4xl font-bold mb-8">Emi Calculator</h1>
+          <div>
+            <h2 className="font-bold text-xl">Total Cost of Asset</h2>
+            <input
+              type="number"
+              value={cost}
+              onChange={ (e) => setCost(e.target.value)}
+              placeholder="total cost"
+            />
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
