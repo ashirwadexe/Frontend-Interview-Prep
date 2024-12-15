fb368c3349cda0d8b086c62ce6bdc9eb9b2f488e
ashirwadexe <ashirwadbappy@gmail.com>
2024-12-15 07:07:23 +0530

    add stopwatch time

---
+++ 06-stopwatch/src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Counter = () => {
+  return (
+    <div className='countdown-timer'>
+        <h1>Stopwatch</h1>
+    </div>
+  )
+}
+
+export default Counter
