840756d5cd88dd93732687bf54187976a6e2744d
ashirwadexe <ashirwadbappy@gmail.com>
2024-12-16 12:04:25 +0530

    accordion component

--- 08-react-experiments/src/App.jsx
+++ 08-react-experiments/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+//------------------------------------------------------------
+//-----How would you show/hide a component in React?"
+//------------------------------------------------------------
+
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggle = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div>App</div>
+    <div>
+      <button onClick={toggle}>
+        {isVisible ? "Hide" : "Show"} Component
+      </button>
+
+      {
+        isVisible && <MyComponent/>
+      }
+    </div>
   )
+}
+
+function MyComponent() {
+  return <h1>Ashirwad</h1>
 }
 
 export default App
