c21dfa0128d24e188fca72be05d327478b9c37f6
ashirwadexe <ashirwadbappy@gmail.com>
2024-12-16 08:06:23 +0530

    solved 3 assignments of input

--- 07-input-learning/src/App.jsx
+++ 07-input-learning/src/App.jsx
@@ -13,8 +13,8 @@
 //   return (
 //     <div>
 //       <h1>Input Learning</h1>
-//       <input 
-//         type="text" 
+//       <input
+//         type="text"
 //         value={inputValue}
 //         onChange={inputHandler}
 //         placeholder='Enter Something...'
@@ -26,29 +26,95 @@
 
 // export default App
 
-
 //UNCONTROLLER INPUT
 
-import React, { useRef } from 'react'
+// import React, { useRef } from 'react'
+
+// const App = () => {
+
+//   const inputRef = useRef(null);
+
+//   const inputHandler = (event) => {
+//     const inputText = inputRef.current.value;
+//     console.log(inputText);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Input Learning</h1>
+//       <input
+//         type="text"
+//         placeholder='enter something here...'
+//         ref={inputRef}
+//       />
+//       <button onClick={inputHandler}>Show Input</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React, { useState } from "react";
+
+// const App = () => {
+
+//   const [inputValue, setInputValue] = useState('');
+
+//   const inputHandler = (event) => {
+//     setInputValue(event.target.value)
+//   }
+//   return (
+//     <div>
+//       <input 
+//         type="text" 
+//         placeholder="enter your name ...."
+//         value={inputValue}
+//         onChange={inputHandler}
+//       />
+//       <h1>hello {inputValue}</h1>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+import React, { useState } from 'react'
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
-  const inputRef = useRef(null);
+  const nameHandler = (event) => {
+    setName(event.target.value);
+  };
 
-  const inputHandler = (event) => {
-    const inputText = inputRef.current.value;
-    console.log(inputText);
-  }
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  };
 
   return (
     <div>
-      <h1>Input Learning</h1>
-      <input
+      <input 
         type="text"
-        placeholder='enter something here...'
-        ref={inputRef} 
+        id='name'
+        placeholder='enter name' 
+        onChange={nameHandler}
+        value={name}
       />
-      <button onClick={inputHandler}>Show Input</button>
+
+      <br />
+      <br />
+
+      <input 
+        type="email" 
+        id='email'
+        placeholder='enetr email'
+        value={email}
+        onChange={emailHandler}
+      />
+
+      <h1>Hello, {name} - Your email: {email}</h1>
     </div>
   )
 }
