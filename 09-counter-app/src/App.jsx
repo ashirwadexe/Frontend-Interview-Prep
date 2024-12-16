83ac9a8a6acf1537229f17454a499916be12a565
ashirwadexe <ashirwadbappy@gmail.com>
2024-12-16 12:16:11 +0530

    completed counter app

--- 09-counter-app/src/App.jsx
+++ 09-counter-app/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount(count-1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Counter App</h1>
+
+      <h3>Count: {count}</h3>
+
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={resetCount}>Reset</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
   )
 }
 
